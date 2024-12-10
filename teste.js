feec00c57aa0f6061e6513afe14fbb4f1dbcd8cb
raqueljustino1 <justinoraquel78@gmail.com>
2024-12-09 22:26:43 -0300

    meu primeiro projeto

---
+++ teste.js
@@ -0,0 +1,23 @@
+var nome= "roberto" //global
+let sobrenome="Carlos"
+
+function func1() {
+    var sobrenome= "carlos"
+    console.log("Olá"+ " "+ nome +" "+ sobrenome)
+
+
+    
+}
+func1()
+//console.log("Olá"+ " "+ nome +" "+ sobrenome)
+
+
+function func2() {
+    var sobrenome= "carlos"
+    console.log("Olá"+ " "+ nome +" "+ "sobrenome")
+
+
+    
+}
+
+func2()
